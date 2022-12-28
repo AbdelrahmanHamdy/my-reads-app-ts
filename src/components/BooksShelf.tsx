ad8ef1b6553623a6fcd30237f55a16d1cb091c5a
Abdelrhman Hamdy <abdelrhman_hamdy2013@hotmail.com>
2022-12-28 08:03:28 +0200

    create BooksShelf component 🔥🦸‍♂️

---
+++ src/components/BooksShelf.tsx
@@ -0,0 +1,28 @@
+
+import BookCard from "./BookCard";
+
+
+/** BooksShelfProps */
+ interface BooksShelfProps {
+    shelfTitle: string;
+     shelf: any[];
+     emitShelfChange: (bookId: string, newShelf: string) => void;
+ }
+
+const BooksShelf = (props: BooksShelfProps) => {
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{props.shelfTitle}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+
+                    {props.shelf.map((book) => <BookCard key={book.id} bookCard={book} onShelfChange={(bookId, newShelf) => {props.emitShelfChange(bookId as string, newShelf)}}/>)}
+
+                </ol>
+            </div>
+        </div>
+
+    );
+}
+
+export default BooksShelf;
